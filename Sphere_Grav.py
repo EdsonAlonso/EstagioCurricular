a929b9488b65a7021f196a50149220db3e469590
EdsonAlonso <41747869+EdsonAlonso@users.noreply.github.com>
2018-07-30 00:20:51 -0300

    Update Sphere_Grav.py
    Supondo 10 gravímetros fiz o programa com valores não muito reais, mas suficientes para verificar o funcionamento do programa

--- Sphere_Grav.py
+++ Sphere_Grav.py
@@ -1,16 +1,44 @@
-xg = float(input('Digite a coordenada x do gravímetro:'))
-yg = float(input('Digite a coordenada y do gravímetro:'))
-zg = float(input('Digite a coordenada z do gravímetro:'))
-xc = float(input( 'Digite a coordenada x do centro da esfera'))
-yc = float(input( 'Digite a coordenada y do centro da esfera'))
-zc = float(input('Digite a coordenada z do centro da esfera'))
-r =  ((xg - xc)**2 + (yg - yc)**2 + (zg - zc)**2)**(1/2)
+#CRIANDO OS GRAVÍMETROS E SUAS POSIÇÕES
+from random import randint
+from random import random
+import numpy as np
+#ng = int(input('Digite o número de gravímetros'))  #VOU COMENTAR ESSE CARA POR ENQUANTO
+min = randint(0,10) #Sorteando a posição inicial do Gravímetro.
+max = randint(0,10) #Sorteando a posição final do Gravímetro.
+
+if min > max:       #Caso o mínimo e o máximo estiverem trocados.
+    min = max
+    max = min
+else:
+    min = min
+    max = max
+if min == max:      #Caso o mínimo e o máximo forem iguais.
+    min = randint(0,max)
+    max = randint(min,10)
+print(min, max)
+x = np.linspace(min, max, num = 10)  #Vetor contendo as posições x do gravímetro.
+coord = list(range(10))   #Estou supondo 10 Gravímetros pois me parece um bom número para conferir o funcionamento do programa.
+for n in range(0,10):
+    coord[n] = (x[n],0)   #A coordenada z vai ser sempre 0, pois os Gravímetros estão na superfície.
+#print(coord)
+
+#DEFININDO ALGUMAS CONSTANTES
 G = 6.674*(10**(-11))
-if r**2 == 0:
-    print('Erro de coordenadas')
-R = float(input('Digite o Raio da esfera:'))
-p = float(input('Digite a densidade da esfera'))
 pi = 3.1415
+
+#CRIANDO A ESFERA DE DENSIDADE P(RHO) E RAIO R NA POSIÇÃO (Xc,Zc)
+R = random()
+p = random()  #Não sei se esses valores estão bons, mas acredito que números entre 0 e 1 são valores razoáveis.
+xc = randint(min,max)
+zc = -10        #Vou deixar esses caras fixos por enquanto, depois posso randomizar as posições deles.
 m = (4*pi*p*R**3)/3
-g = -(G*m)/r**3
-print('g é igual a {}'.format(g))
+
+#CÁLCULO DAS ANOMALIAS MEDIDAS EM CADA GRAVÍMETRO
+r = list(range(10))
+g = list(range(10))
+for n in range(0,10):
+    r[n] = ((xc - x[n])**2 + zc**2)**(1/2)
+    if r[n] == 0:
+        print('Alguma coordenada está errada')
+    g[n] = abs(-(G*m)/r[n]**3)
+print('O vetor das anomalias gravimétricas é {}'.format(g))
